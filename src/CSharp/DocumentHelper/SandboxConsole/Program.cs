520bb591913d8abf24244c0ef526884200d36df5
Christian Popescu <christian.popescu@outlook.com>
2025-05-29 15:51:26 +0200

    AsciidoctorJ tested from C# + documented

---
+++ src/CSharp/DocumentHelper/SandboxConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using SandboxConsole.Workspace;
+using System.Diagnostics;
+
+namespace SandboxConsole;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Wrk_01_Asciidoc_to_html_and_pdf.Run();
+
+
+
+    }
+}
---
+++ src/CSharp/DocumentHelper/SandboxConsole/Workspace/Wrk-01-Asciidoc-to-html-and_pdf.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SandboxConsole.Workspace
+{
+    class Wrk_01_Asciidoc_to_html_and_pdf
+    {
+
+        public static void Run()
+        {
+            Console.WriteLine("Sanbox Console");
+
+            var process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.Arguments = "/c asciidoctorj.bat -b pdf Qt.adoc";
+            process.StartInfo.WorkingDirectory = @"D:\ccp_wrks\KnowledgeManagement\content\GuiFrameworks\Qt";
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            Console.WriteLine(output);
+        }
+    }
+}
